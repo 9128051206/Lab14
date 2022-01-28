c81cadfed48d3ffff22d4cbbf6e8190db6e7373d
Юлия Шепелева <9128051206@mail.ru>
2022-01-28 18:48:18 +0500

    Добавьте файлы проекта.

---
+++ Lab14/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set;}
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract string Say();
+
+        public string ShowInfo()
+        {
+            return $"{Name} \n{Say()}"; 
+        }
+
+    }
+    class Cat : Animal
+    {        
+        private string name;
+        public override string Name 
+        { 
+            get => name;
+            set => name=value; 
+        }
+        public Cat (string name)
+            :base(name)
+        {
+            
+        }
+
+        public override string Say()
+        {
+            return "Мяу";
+        }
+    }
+    class Dog : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name)
+            : base(name)
+        {
+
+        }
+
+        public override string Say()
+        {
+            return "Гав";
+        }
+    }
+}
---
+++ Lab14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            Cat cat = new Cat("Мурка");
+            Console.WriteLine(cat.ShowInfo());
+            Dog dog = new Dog("Шарик");
+            Console.WriteLine(dog.ShowInfo());
+            Console.ReadKey();
+        }
+    }
+}
